be676ff8708f8822716f9c08bba3d020b61b1c34
tony <antoabrand@gmail.com>
2017-12-26 14:44:31 -0600

    adding concurrency, synchronization practice

---
+++ src/tony/concurrency/Concurrency.java
@@ -0,0 +1,19 @@
+package tony.concurrency;
+
+public class Concurrency {
+
+	public static void main(String[] args) {
+
+		Runnable runnable = () -> {
+			System.out.println("Current thread you are running on is: " + Thread.currentThread().getName());
+		};
+
+		Thread t1 = new Thread(runnable);
+		t1.setName("My thread");
+		t1.start();
+		
+		//will run thread and display the main thread and not My Thread
+		t1.run();
+	}
+
+}
