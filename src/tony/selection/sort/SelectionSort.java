0fb1008f89575c7c05956a2816f805e39b718125
antoabrand <antoabrand@gmail.com>
2018-03-11 22:22:23 -0500

    adding select sort implementation

---
+++ src/tony/selection/sort/SelectionSort.java
@@ -0,0 +1,39 @@
+package tony.selection.sort;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		
+		int intArray [] = {1,33,5,-39,23,100};
+		
+		for(int lastUnsorted = intArray.length -1; lastUnsorted > 0 ; lastUnsorted --) {
+			
+			int largest = 0;
+			
+			for(int inner = 1; inner <= lastUnsorted; inner++) {
+				if(intArray[inner] > intArray[largest]) {
+					largest = inner; 
+				}
+				swap(intArray, largest, lastUnsorted);
+			}
+			
+		}
+		
+		//display what is in the array now
+		for(int i : intArray) {
+			System.out.print(i+",");
+		}
+
+	}
+    //stores first element value in a temp variable so as to not lose during swapping
+    public static void swap(int[] array, int index, int index2) {
+        if (index == index2) {
+            return;
+        }
+
+        int temp = array[index];
+        array[index] = array[index2];
+        array[index2] = temp;
+    }
+
+}
