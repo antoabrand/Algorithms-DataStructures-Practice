1d347601fa72f142738c504ddc22ed5e48a3ce5d
Tony Andrade <aandrade0205@gmail.com>
2020-12-06 13:08:16 -0600

    Solved: 1 bit 2 bit

---
+++ src/com/code/leet/OneAndTwoBits.java
@@ -0,0 +1,35 @@
+package com.code.leet;
+
+/**
+ * Description: We have two special characters. The first character can be
+ * represented by one bit 0. The second character can be represented by two bits
+ * (10 or 11).
+ * 
+ * Now given a string represented by several bits. Return whether the last
+ * character must be a one-bit character or not. The given string will always
+ * end with a zero.
+ */
+public class OneAndTwoBits {
+
+	public static void main(String[] args) {
+		int[] test = { 1, 0 };
+		int[] test2 = { 1, 1, 0 };
+
+		System.out.println(solution(test));
+		System.out.println(solution(test2));
+	}
+
+	private static boolean solution(int[] bits) {
+		boolean answer = true;
+
+		for (int i = 0; i < bits.length; i++) {
+			if (bits[i] == 1) {
+				answer = false;
+				i++;
+			} else {
+				answer = true;
+			}
+		}
+		return answer;
+	}
+}
